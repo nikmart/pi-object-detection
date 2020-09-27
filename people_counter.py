2f500ce02e9bffead4980c5fe56eabe95223685b
Nik Martelaro <nmartelaro@gmail.com>
2020-09-27 18:16:55 -0400

    updating for 2020

--- people_counter.py
+++ people_counter.py
@@ -1,14 +1,36 @@
 # USAGE
-# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# python pi_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
+from multiprocessing import Process
+from multiprocessing import Queue
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
+
+def classify_frame(net, inputQueue, outputQueue):
+	# keep looping
+	while True:
+		# check to see if there is a frame in our input queue
+		if not inputQueue.empty():
+			# grab the frame from the input queue, resize it, and
+			# construct a blob from it
+			frame = inputQueue.get()
+			frame = cv2.resize(frame, (300, 300))
+			blob = cv2.dnn.blobFromImage(frame, 0.007843,
+				(300, 300), 127.5)
+
+			# set the blob as input to our deep learning object
+			# detector and obtain the detections
+			net.setInput(blob)
+			detections = net.forward()
+
+			# write the detections to the output queue
+			outputQueue.put(detections)
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,8 +40,6 @@ ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
-ap.add_argument("-v", "--video_source", type=int, default=0,
-	help="video source (default = 0, external usually = 1)")
 args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
@@ -34,46 +54,68 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
+# initialize the input queue (frames), output queue (detections),
+# and the list of actual detections returned by the child process
+inputQueue = Queue(maxsize=1)
+outputQueue = Queue(maxsize=1)
+detections = None
+
+# construct a child process *indepedent* from our main process of
+# execution
+print("[INFO] starting process...")
+p = Process(target=classify_frame, args=(net, inputQueue,
+	outputQueue,))
+p.daemon = True
+p.start()
+
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
-vs = VideoStream(src=args["video_source"]).start()
+vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# grab the frame from the threaded video stream, resize it, and
+	# grab its imensions
 	frame = vs.read()
-	frame = imutils.resize(frame, width=800)
+	frame = imutils.resize(frame, width=400)
+	(fH, fW) = frame.shape[:2]
 
-	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+	# if the input queue *is* empty, give the current frame to
+	# classify
+	if inputQueue.empty():
+		inputQueue.put(frame)
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	# if the output queue *is not* empty, grab the detections
+	if not outputQueue.empty():
+		detections = outputQueue.get()
 
+	# initialize the people counter for each frame
 	people_count = 0
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
+	# check to see if our detectios are not None (and if so, we'll
+	# draw the detections on the frame)
+	if detections is not None:
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated
+			# with the prediction
+			confidence = detections[0, 0, i, 2]
+
+			# filter out weak detections by ensuring the `confidence`
+			# is greater than the minimum confidence
+			if confidence < args["confidence"]:
+				continue
+
+			# otherwise, extract the index of the class label from
+			# the `detections`, then compute the (x, y)-coordinates
+			# of the bounding box for the object
 			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			dims = np.array([fW, fH, fW, fH])
+			box = detections[0, 0, i, 3:7] * dims
 			(startX, startY, endX, endY) = box.astype("int")
 
 			# draw the prediction on the frame
@@ -84,13 +126,11 @@ while True:
 			y = startY - 15 if startY - 15 > 15 else startY + 15
 			cv2.putText(frame, label, (startX, y),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
 			# count the people
 			if CLASSES[idx] == "person":
 				people_count += 1
-
-	cv2.putText(frame, str(people_count), (250, 250),
-		cv2.FONT_HERSHEY_SIMPLEX, 10, (144,0,255), 10)
+		cv2.putText(frame, str(people_count), (250, 250),
+			cv2.FONT_HERSHEY_SIMPLEX, 10, (144,0,255), 10)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
